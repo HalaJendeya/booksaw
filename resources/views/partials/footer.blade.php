c3b630d0febd591eff06ac7a23a49bd6cacbb123
Hala Jendeya <halahaithamjendeya@gmail.com>
2026-05-01 19:03:59 +0300

    updated footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -78,20 +78,8 @@
 
                         <div class="col-md-6">
                             <p>
-                                © 2022 All rights reserved. Free HTML Template by
-                                <a href="https://www.templatesjungle.com/" target="_blank">TemplatesJungle</a>
+                                © 2022 All rights reserved.
                             </p>
-                        </div>
-
-                        <div class="col-md-6">
-                            <div class="social-links align-right">
-                                <ul>
-                                    <li><a href="#"><i class="icon icon-facebook"></i></a></li>
-                                    <li><a href="#"><i class="icon icon-twitter"></i></a></li>
-                                    <li><a href="#"><i class="icon icon-youtube-play"></i></a></li>
-                                    <li><a href="#"><i class="icon icon-behance-square"></i></a></li>
-                                </ul>
-                            </div>
                         </div>
 
                     </div>
